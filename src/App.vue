ec81c217fc37f0d7fd9d7102beb7eab24ee6a007
mohammed-allou <60431576+mohammed-allou@users.noreply.github.com>
2020-10-10 12:24:23 +0100

    mise en page

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,59 @@
 <template>
-<v-app>
-    <v-app-bar app color="gray" dark>
-        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+  <v-app>
+    <v-app-bar app color="teal darken-3" dark>
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
 
-        <v-toolbar-title>Produits</v-toolbar-title>
+      <v-toolbar-title>Produits</v-toolbar-title>
 
-        <v-spacer></v-spacer>
+      <v-spacer></v-spacer>
 
-        <v-btn icon>
-            <v-icon>mdi-magnify</v-icon>
-        </v-btn>
-        <v-spacer></v-spacer>
-        <v-btn icon>
-            <v-icon>mdi-heart</v-icon>
-        </v-btn>
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+      <v-spacer></v-spacer>
+      <v-btn icon>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
     </v-app-bar>
 
     <v-main>
-        {{ this.$store.state.test }}
-        <products />
+      {{ this.$store.state.test }}
+      <products />
     </v-main>
-</v-app>
+
+    <v-footer padless>
+      <v-card class="flex">
+        <v-card-title class="darken-1">
+          <v-spacer></v-spacer>
+
+          <v-btn v-for="icon in icons" :key="icon" class="mx-4" icon>
+            <v-icon size="24px" class="teal--text">{{ icon }}</v-icon>
+          </v-btn>
+        </v-card-title>
+      </v-card>
+    </v-footer>
+  </v-app>
 </template>
 
 <script>
 import products from "./components/products";
-import {
-    mapState
-} from "vuex";
+import { mapState } from "vuex";
 
 export default {
-    name: "App",
+  name: "App",
 
-    components: {
-        products,
-    },
-    mounted() {
-        this.$store.dispatch("products_");
-    },
+  components: {
+    products,
+  },
+  mounted() {
+    this.$store.dispatch("products_");
+  },
 
-    computed: {
-        ...mapState(["products"]),
-    },
-    data: () => ({}),
+  computed: {
+    ...mapState(["products"]),
+  },
+  data: () => ({
+    icons: ["mdi-facebook", "mdi-twitter", "mdi-linkedin", "mdi-instagram"],
+  }),
 };
 </script>
--- src/components/products.vue
+++ src/components/products.vue
@@ -1,40 +1,51 @@
 <template>
-  <v-container fluid>
-    <v-row>
-      <v-col class="d-flex justify-space-between">
-        <v-card v-for="product in products" :key="product.id">
-          <v-img height="200" :src="product.img"></v-img>
-          <v-card-title>{{ product.nom }}</v-card-title>
-          <v-card-text>
-            <div class="my-4 subtitle-1">
-              Quantité Restante : {{ product.quantite }} kg
-            </div>
-          </v-card-text>
-        </v-card>
-      </v-col>
-    </v-row>
-  </v-container>
+  <div class="home">
+    <v-container class="teal lighten" fluid >
+      <v-row dense>
+        <v-col class="d-flex justify-space-between"  >
+          <v-card
+            class="pa-3"
+            outlined
+            v-for="product in products"
+            :key="product.id"
+          >
+            <v-img height="200" contain :src="product.img"></v-img>
+
+            <v-card-title class="text-center">{{ product.nom }}</v-card-title>
+
+            <v-card-text>
+              <div class="my-4 subtitle-1">
+                Quantité Restante : {{ product.quantite }} kg
+              </div>
+            </v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
 </template>
 
 <script>
 import Vue from "vue";
 import axios from "axios";
 import VueAxios from "vue-axios";
-import VueResource from 'vue-resource'
-Vue.use(axios,VueAxios,VueResource);
+import VueResource from "vue-resource";
+Vue.use(axios, VueAxios, VueResource);
 export default {
   name: "products",
 
   data: () => ({
     products: [],
+    icons: ["mdi-facebook", "mdi-twitter", "mdi-linkedin", "mdi-instagram"],
+    justify: ["start", "end", "center", "space-between", "space-around"],
   }),
   mounted() {
-       
-    axios.get("https://e503eef4-bd94-4dd6-9fb0-d6719bc06ea7.mock.pstmn.io/products")
+    axios
+      .get(
+        "https://e503eef4-bd94-4dd6-9fb0-d6719bc06ea7.mock.pstmn.io/products"
+      )
       .then((response) => {
-        console.log(response.data);
         this.products = response.data;
-        
       })
       .catch((error) => {
         console.log(error);
