9239f6ea8b4c4417a037462203ec79d90749ff7c
mohammed-allou <60431576+mohammed-allou@users.noreply.github.com>
2020-10-22 12:57:11 +0100

    update

--- src/components/products.vue
+++ src/components/products.vue
@@ -1,34 +1,28 @@
 <template>
-  <div class="home">
-    <v-container class="teal lighten" fluid>
-      <v-row dense>
-        <v-col class="d-flex justify-space-between">
-          <div v-if="isError">
-            <v-alert color="red" dismissible type="success">{{
-              error
-            }}</v-alert>
+  <v-timeline>
+    <v-timeline-item  v-for="product in products"
+        :key="product.id" >
+      <div v-if="isError">
+        <v-alert color="red" dismissible type="success">{{ error }}</v-alert>
+      </div>
+      <v-card
+        v-else
+        class="elevation-5 mx-auto"
+         max-width="360"
+       
+      >
+        <v-img height="200" contain :src="product.img"></v-img>
+
+        <v-card-title class="headline">{{ product.name }}</v-card-title>
+
+        <v-card-text>
+          <div class="my-4 subtitle-1">
+            Quantité Restante : {{ product.quantity }} {{ product.unit }}
           </div>
-          <v-card
-            v-else
-            class="pa-3"
-            outlined
-            v-for="product in products"
-            :key="product.id"
-          >
-            <v-img height="200" contain :src="product.img"></v-img>
-
-            <v-card-title class="text-center">{{ product.name }}</v-card-title>
-
-            <v-card-text>
-              <div class="my-4 subtitle-1">
-                Quantité Restante : {{ product.quantity }} {{ product.unit }}
-              </div>
-            </v-card-text>
-          </v-card>
-        </v-col>
-      </v-row>
-    </v-container>
-  </div>
+        </v-card-text>
+      </v-card>
+    </v-timeline-item>
+  </v-timeline>
 </template>
 
 <script>
--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,1 @@
-export const URL_API_PRODUCT = "https://866e8e87-f8b1-4a35-8bde-61daad340cd8.mock.pstmn.io" 
+export const URL_API_PRODUCT = "http://localhost:9090" 
